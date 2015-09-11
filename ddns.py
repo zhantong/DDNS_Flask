adefd5f64b8727cf45a10d91b6a544098bfc5828
zhantong1994 <zhantong1994@163.com>
2015-09-11 21:19:11 +0800

    增加对DNSPod更新部分

--- ddns.py
+++ ddns.py
@@ -6,10 +6,17 @@ application=Flask(__name__)
 @application.route('/ipv6')
 def ipv6():
 	ip=request.remote_addr
+	with open('/etc/nginx/sites-available/default','r') as f:
+		s=f.read()
+	r=re.compile(r'http://\[(.*?)\]; \# for raspberry pi')
+	s=s.replace(r.findall(s)[0],ip)
+	with open('/etc/nginx/sites-available/default','w') as f:
+		f.write(s)
+	os.popen('service nginx restart')
 	url='https://dnsapi.cn/Record.Modify'
 	info={
-		'login_email':'102517642@qq.com',
-		'login_password':'cipherpanzer',
+		'login_email':'xxx',
+		'login_password':'xxx',
 		'format':'json',
 		'domain_id':'23606146',
 		'record_id':'116841773',
@@ -18,17 +25,10 @@ def ipv6():
 		'record_type':'AAAA',
 		'value':ip
 		}
-	with open('/etc/nginx/sites-available/default','r') as f:
-		s=f.read()
-	r=re.compile(r'http://\[(.*?)\]; \# for raspberry pi')
-	s=s.replace(r.findall(s)[0],ip)
-	with open('/etc/nginx/sites-available/default','w') as f:
-		f.write(s)
-	os.popen('service nginx restart')
-	#data=urllib.parse.urlencode(info).encode()
-	#t=urllib.request.urlopen(url,data=data)
-	#return t.read().decode('utf-8')
-	#return s
+
+	data=urllib.parse.urlencode(info).encode()
+	t=urllib.request.urlopen(url,data=data)
+	return t.read().decode('utf-8')
 	return s
 
 if __name__=='__main__':
